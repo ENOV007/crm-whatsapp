e3f2219a8f321bdb22219c87d9a418ef88b100ce
ENOV007 <eduardonoviaski7@gmail.com>
2026-06-12 13:08:51 -0300

    Use PORT env var for nginx listen port (Railway compat)

---
+++ frontend/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
